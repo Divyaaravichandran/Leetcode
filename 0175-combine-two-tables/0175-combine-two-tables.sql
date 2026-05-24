8874f3a60fc37b87183f5cb09c17d7d772c85471
Divya <divyar.23it@kongu.edu>
2026-05-24 09:04:13 +0530

    Time: 416 ms (52.37%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,5 +1,7 @@
-# Write your MySQL query statement below
-SELECT p.firstName,p.lastName,a.city,a.state
-FROM Person p
-LEFT JOIN Address a
-ON p.personId = a.personId;
+SELECT
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM Address a
+RIGHT JOIN Person p ON a.personId = p.personId;
