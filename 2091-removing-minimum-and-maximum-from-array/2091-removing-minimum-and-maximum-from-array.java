b56e6271439357a5605078e157cd594077c1efd7
Divya <divyar.23it@kongu.edu>
2026-09-01 09:44:06 +0530

    Time: 5 ms (5.27%), Space: 86.7 MB (68.14%) - LeetHub

---
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -0,0 +1,79 @@
+import java.util.*;
+class Solution {
+    public int minimumDeletions(int[] nums) {
+        int min = nums[0];
+        int max = nums[0];
+        int minindex = 0;
+        int maxindex = 0;
+        int ans1 = 1;
+        int ans2 = 1;
+        int ans3 = 2;
+        int ans;
+        if(nums.length==1){
+            return 1;
+        }
+        if(nums.length==2){
+            return 2;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<min){
+                min = nums[i];
+                minindex = i;
+            }
+            if(nums[i]>max){
+                max = nums[i];
+                maxindex = i;
+            }
+        }
+
+        //Delete from first
+        if(minindex<maxindex)
+            ans1 += maxindex;
+        else if(minindex>maxindex)
+            ans1 += minindex;
+        System.out.println(ans1);
+
+        //Delete from end
+        if(minindex<maxindex){
+            for(int i=nums.length-1;i>minindex;i--){
+                ans2+=1;
+            }
+        }
+        else if(minindex>maxindex){
+            for(int i=nums.length-1;i>maxindex;i--){
+                ans2+=1;
+            }
+        }
+        System.out.println(ans2);
+
+        //Delete from start and end
+        if(minindex<maxindex){
+            ans3+=minindex;
+            for(int i=nums.length-1;i>maxindex;i--){
+                ans3+=1;
+            }
+
+        }
+        else if(minindex>maxindex){
+            ans3+=maxindex;
+            for(int i=nums.length-1;i>minindex;i--){
+                ans3+=1;
+            }
+        }
+        System.out.println(ans3);
+
+        if(ans1<=ans2 && ans1<=ans3){
+            ans = ans1;
+            return ans;
+        }
+        if(ans2<=ans1 && ans2<=ans3){
+            ans = ans2;
+            return ans;
+        }
+        if(ans3<=ans1 && ans3<=ans2){
+            ans = ans3;
+            return ans;
+        }
+        return 0;
+    }
+}
