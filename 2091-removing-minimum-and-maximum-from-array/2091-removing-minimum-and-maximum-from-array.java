c634fd4b698c99ec01a9870b74b8f9a7699846a4
Divya <divyar.23it@kongu.edu>
2026-09-01 10:06:19 +0530

    Time: 3 ms (69.78%), Space: 86.8 MB (49.75%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -1,61 +1,23 @@
 import java.util.*;
 class Solution {
     public int minimumDeletions(int[] nums) {
-        int ans;
         int minindex = 0;
         int maxindex = 0;
-        int ans1 = 1;
-        int ans2 = 1;
-        int ans3 = 2;
-        if(nums.length==1){
-            return 1;
-        }
-        if(nums.length==2){
-            return 2;
-        }
+
         for(int i=0;i<nums.length;i++){
-            if(nums[i]<nums[minindex]){
+            if(nums[i]<nums[minindex])
                 minindex = i;
-            }
-            if(nums[i]>nums[maxindex]){
+            if(nums[i]>nums[maxindex])
                 maxindex = i;
-            }
         }
 
-        //Delete from first
-        if(minindex<maxindex)
-            ans1 += maxindex;
-        else if(minindex>maxindex)
-            ans1 += minindex;
+        int left = Math.min(minindex, maxindex);
+        int right = Math.max(minindex, maxindex);
 
-        //Delete from end
-        if(minindex<maxindex){
-            for(int i=nums.length-1;i>minindex;i--){
-                ans2+=1;
-            }
-        }
-        else if(minindex>maxindex){
-            for(int i=nums.length-1;i>maxindex;i--){
-                ans2+=1;
-            }
-        }
-
-        //Delete from start and end
-        if(minindex<maxindex){
-            ans3+=minindex;
-            for(int i=nums.length-1;i>maxindex;i--){
-                ans3+=1;
-            }
-
-        }
-        else if(minindex>maxindex){
-            ans3+=maxindex;
-            for(int i=nums.length-1;i>minindex;i--){
-                ans3+=1;
-            }
-        }
-
-        ans = Math.min(ans1,ans2);
-        return Math.min(ans,ans3);
+        int f = right+1;
+        int b = nums.length-left;
+        int fb = (left+1) + (nums.length-right);
+        
+        return Math.min(f,Math.min(b,fb));
     }
 }
